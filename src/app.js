1ec1eb24127a376178d4783986746bf45f8ea287
Marina Master <marina777master@mail.ru>
2023-08-07 21:10:56 -0400

    complite domain-name-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function combinations() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions =[".com", ".net", ".us", ".io", ".etc"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let z = 0; z < adjs.length; z++) {
+      for (let y = 0; y < nouns.length; y++) {
+        for (let x =0; x <extensions.length; x++){
+        console.log(`${pronouns[i]}${adjs[z]}${nouns[y]}${extensions[x]}`);
+        }
+      }
+    }
+  }
+}
+combinations();
